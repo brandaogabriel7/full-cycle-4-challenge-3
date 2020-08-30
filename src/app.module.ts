8ffef23293fe7636b37cd78006b0a0f3259df77a
Gabriel Brandão <gabrielsb@take.net>
2020-08-30 17:14:04 -0300

    Delete app service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [CategoriesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
