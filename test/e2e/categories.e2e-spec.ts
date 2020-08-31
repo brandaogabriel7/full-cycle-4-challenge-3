2983252045d7fe4b21e26993f46d473a8f18d364
Gabriel Brandão <gabrielsb@take.net>
2020-08-31 11:39:36 -0300

    Create e2e tests for categories

--- test/e2e/categories.e2e-spec.ts
+++ test/e2e/categories.e2e-spec.ts
@@ -1,37 +1,61 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
 import * as request from 'supertest';
-import { CategoriesModule } from './../../src/categories/categories.module';
+import { CategoriesModule } from '../../src/categories/categories.module';
+import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Category } from '../../src/categories/dto/category.entity';
 
-describe('CategoriesController (e2e)', () => {
+describe('Categories', () => {
   let app: INestApplication;
+  let categoriesRepository: Repository<Category>;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [CategoriesModule]
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        CategoriesModule,
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: 'test_db',
+          autoLoadEntities: true,
+          synchronize: true,
+          dropSchema: true
+        })
+      ]
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
     await app.init();
+
+    categoriesRepository = await moduleRef.get(getRepositoryToken(Category));
   });
 
-  const expectedCategories = [
-    { id: 1, name: 'Category 1' },
-    { id: 2, name: 'Category 2' },
-    { id: 3, name: 'Category 3' },
-    { id: 4, name: 'Category 4' },
-    { id: 5, name: 'Category 5' },
-    { id: 6, name: 'Category 6' },
-    { id: 7, name: 'Category 7' },
-    { id: 8, name: 'Category 8' },
-    { id: 9, name: 'Category 9' },
-    { id: 10, name: 'Category 10' }
-  ];
+  it(`/GET categories`, async () => {
+    const result = [{ id: 1, name: 'Category 1' }];
+    await categoriesRepository.save(result);
 
-  it('/categories (GET)', () => {
-    return request(app.getHttpServer())
+    request(app.getHttpServer())
       .get('/categories')
       .expect(200)
-      .expect(expectedCategories);
+      .expect(result);
+  });
+
+  it('POST categories', async done => {
+    request(app.getHttpServer())
+      .post('/categories')
+      .send({ name: 'Category 1' })
+      .expect('Content-type', /json/)
+      .expect(201)
+      .end(err => {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  afterEach(async () => {
+    await categoriesRepository.query('DELETE from category');
+  });
+  afterAll(async () => {
+    await app.close();
   });
 });
